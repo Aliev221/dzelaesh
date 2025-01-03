3e0b53dbffd53a0afedd2abeadf054c2614934bd
Aliev221 <islamgiga05@gmail.com>
2025-01-03 15:06:14 +0300

    Add files via upload

---
+++ BoostGram-Bot-main/config.py
@@ -0,0 +1,14 @@
+from aiogram import Bot
+from aiogram.client.bot import DefaultBotProperties
+
+BOT_TOKEN = '7864285687:AAEQImEEi9wyM1azKoEGHrRXypN3mUyXPSU'
+CRYPTO_TOKEN = ''
+
+ADMIN = 65092173892
+SUPPORT_URL = 'https://t.me/FsEagles'
+CHANNEL_URL = 'https://t.me/fhtsghsrtf'
+
+API_KEY = 'bMXCwBifT1MdSsSALz8Zafs4KlCUBzZuhEXdudPG0QPnINSvQUws8J0yUAAD'
+
+
+bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode='HTML'))
